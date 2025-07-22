d642bcb894f747b06db6c822ce26d867882fd017
ShaunZap <sjbzappy@gmail.com>
2025-07-22 15:22:22 +0530

    bug fix

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -18,7 +18,7 @@ export default function Results() {
 
   useEffect(() => {
     if (data) {
-      setLoading(true);
+      setLoading(false);
       console.log("Data loaded:", data);
     }
   }, [data]);
